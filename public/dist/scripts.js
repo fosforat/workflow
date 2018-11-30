445213ee936c435d28b80890b9c1dca7c39ff9f5
Murat Dalgıç <mdalgic@DOL.int>
2018-11-30 16:55:49 +0300

    Edit

--- public/dist/scripts.js
+++ public/dist/scripts.js
@@ -1,2 +1,2 @@
-"use strict";function _classCallCheck(e,n){if(!(e instanceof n))throw new TypeError("Cannot call a class as a function")}var _createClass=function(){function e(e,n){for(var t=0;t<n.length;t++){var r=n[t];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}return function(n,t,r){return t&&e(n.prototype,t),r&&e(n,r),n}}(),Person=function(){function e(n){_classCallCheck(this,e),this.name=n}return _createClass(e,[{key:"hello",value:function(){return"string"==typeof this.name?"Hello, I am "+this.name+"!":"Hello"}}]),e}(),person=new Person("Wekios"),greetHTML=templates.greeting({message:person.hello()});document.write(greetHTML);
+"use strict";
 "use strict";for(var max=10,i=0;i<max;i++)console.log(i);
